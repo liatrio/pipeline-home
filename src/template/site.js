2fd0b978fa5bc2d2fc07bd1f049113314e7281bc
Jeremy Hayes <jeremyh@liatrio.com>
2018-06-28 10:26:14 -0400

    PIPE-45 add more buttons/logos and a link builder

--- src/template/site.js
+++ src/template/site.js
@@ -1,9 +1,20 @@
 const site = {
   title: 'Demo Pipeline',
-  subtitle: 'Homepage',
+  project_name: 'PROJECT_NAME',
+  project_key: 'PROJECT_KEY',
+  app_name: 'APP_NAME',
+  logo_file: 'logo.png',
+  tools_domain: 'liatr.io',
   footer: 'Fail fast.',
   slack: 'https://example.slack.com',
-  email: 'mailto:support@example.com'
+  email: 'mailto:support@example.com',
+  jenkins_base: 'http://jenkins.liatr.io',
+  bitbucket_base: 'http://bitbucket.liatr.io',
+  jira_base: 'http://jira.liatr.io',
+  confluence_base: 'http://confluence.liatr.io',
+  artifactory_base: 'http://artifactory.liatr.io',
+  sonarqube_base: 'http://sonarqube.liatr.io',
+  chat_base: 'http://liatrio-demo.slack.com'
 };
 
 export default site;
