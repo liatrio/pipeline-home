a849c6d6426f71fea2177c32c32d8dfa4d36c8a7
Jeremy Hayes <jphayes@users.noreply.github.com>
2019-01-08 12:37:19 -0500

    Old pipeline -> new AWS (#8)
    * new AWS structure
    
    * add variable
    
    * new AWS structure
    
    * change region
    
    * adjust policy
    
    * adjust policy
    
    * rename crediental

--- dashboard.tf
+++ dashboard.tf
@@ -1,34 +1,56 @@
-variable "bucket_name" {}
+variable "app_name" {
+  description = "Name given to this application instance"
+}
+
+variable "bucket_name" {
+  description = "Name of the bucket to store the dashboard files"
+}
+
+variable "jenkins_user" {
+  description = "User who kicked off the Jenkins job"
+}
 
 terraform {
   backend "s3" {
-    bucket = "liatristorage"
-    key    = "liatristorage/dashboard.tfstate"
-    region = "us-west-2"
+    bucket               = "pipeline-state.liatr.io"
+    region               = "us-east-1"
+    key                  = "pipeline-dashboard.tfstate"
+    workspace_key_prefix = "demo"
+    dynamodb_table       = "pipeline-state-lock"
+    encrypt              = "true"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  version = "~> 1.54"
 }
 
 resource "aws_s3_bucket" "b" {
-  bucket = "${var.bucket_name}"
-  acl    = "public-read"
+  bucket        = "${var.bucket_name}"
+  acl           = "public-read"
   force_destroy = true
+
   policy = <<POLICY
 {
-    "Version":"2012-10-17",
-    "Statement":[{
+  "Version":"2012-10-17",
+  "Statement":[
+    {
       "Sid":"PublicReadGetObject",
-          "Effect":"Allow",
-        "Principal": "*",
-        "Action":["s3:GetObject"],
-        "Resource":["arn:aws:s3:::${var.bucket_name}/*"
-        ]
-      }
-    ]
-  }
+      "Effect":"Allow",
+      "Principal": "*",
+      "Action":["s3:GetObject"],
+      "Resource":["arn:aws:s3:::${var.bucket_name}/*"]
+    },
+    {
+      "Sid":"JenkinsPutObject",
+      "Effect":"Allow",
+      "Principal":{"AWS":["arn:aws:iam::003744521125:user/SVC-Jenkins"]},
+      "Action":["s3:PutObject"],
+      "Resource":["arn:aws:s3:::${var.bucket_name}/*"]
+    }
+  ]
+}
 POLICY
 
   website {
@@ -45,6 +67,14 @@ POLICY
     }
 }]
 EOF
+  }
+
+  tags {
+    Environment  = "demo"
+    Client       = "liatrio"
+    Project      = "Demo Pipeline"
+    Owner        = "${var.jenkins_user}"
+    DemoPipeline = "${var.app_name}"
   }
 }
 
